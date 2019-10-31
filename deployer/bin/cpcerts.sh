2aea106d2670ffff42cbcb038d02e4e7a45d06d9
antoinehage <me@antoinehage.com>
2019-10-31 16:32:06 -0400

    added bin sh file to copy certs

---
+++ deployer/bin/cpcerts.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ -f "/opt/soajs/certificates/domains" ]; then
+	domain=$(cat /opt/soajs/certificates/domains | awk -F, '{ print $1 }')
+	if [ -f "/opt/soajs/letsencrypt/live/$domain/privkey.pem" ]; then
+		certbot renew
+        cp "/opt/soajs/letsencrypt/live/$domain/privkey.pem" /opt/soajs/certificates/privkey.pem
+        cp "/opt/soajs/letsencrypt/live/$domain/fullchain.pem" /opt/soajs/certificates/fullchain.pem
+    else
+        echo $'Unable to renew certificate, file not found @ /opt/soajs/letsencrypt/live/'${domain}
+	fi
+fi
